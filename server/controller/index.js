5e4a8671892f4b744c38e13121f67f136c2a12e9
Anirudh919 <101653588+Anirudh919@users.noreply.github.com>
2023-06-28 17:47:33 +0530

    new mail add

--- server/controller/index.js
+++ server/controller/index.js
@@ -13,14 +13,14 @@ const sendMail = async (req, res) => {
 
   let info = await transporter.sendMail({
     from: req.body.sender,
-    to: "gautamyadav1992002@gmail.com",
+    to: "anirudhkashyap67@@gmail.com",
     subject: "PortFolio Message",
 
     text: ` from ${req.body.senderEmail} , name : ${req.body.name} and the message is ${req.body.message}`,
     html: `<h3>from</h3> :  <h3> ${req.body.senderEmail}</h3>  <p>name :  ${req.body.name} and the message is ${req.body.message}</p>`,
   });
   let info2 = await transporter.sendMail({
-    from: "gautamyadav1992002@gmail.com",
+    from: "anirudhkashyap67@@gmail.com",
     to: req.body.senderEmail,
     subject: "feed-back message",
     text: `Your Message has been successfully send to Gautam Yadav ,
