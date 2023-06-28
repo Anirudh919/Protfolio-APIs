e7bf31f72a1297889931f14eac99b637c30de8d1
Anirudh919 <101653588+Anirudh919@users.noreply.github.com>
2023-06-28 17:46:25 +0530

    updaate

--- server/controller/index.js
+++ server/controller/index.js
@@ -15,17 +15,17 @@ const sendMail = async (req, res) => {
     from: req.body.sender,
     to: "gautamyadav1992002@gmail.com",
     subject: "PortFolio Message",
-    text: `testing api`,
-    // text: ` from ${req.body.senderEmail} , name : ${req.body.name} and the message is ${req.body.message}`,
-    // html: `<h3>from</h3> :  <h3> ${req.body.senderEmail}</h3>  <p>name :  ${req.body.name} and the message is ${req.body.message}</p>`,
+
+    text: ` from ${req.body.senderEmail} , name : ${req.body.name} and the message is ${req.body.message}`,
+    html: `<h3>from</h3> :  <h3> ${req.body.senderEmail}</h3>  <p>name :  ${req.body.name} and the message is ${req.body.message}</p>`,
   });
-  // let info2 = await transporter.sendMail({
-  //   from: "gautamyadav1992002@gmail.com",
-  //   to: req.body.senderEmail,
-  //   subject: "feed-back message",
-  //   text: `Your Message has been successfully send to Gautam Yadav ,
-  //   ${req.body.message}`,
-  // });
+  let info2 = await transporter.sendMail({
+    from: "gautamyadav1992002@gmail.com",
+    to: req.body.senderEmail,
+    subject: "feed-back message",
+    text: `Your Message has been successfully send to Gautam Yadav ,
+    ${req.body.message}`,
+  });
   res.json(info);
 };
 
