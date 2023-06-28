0b50a9d5d07c20c707cdac306d05647406c0f879
Anirudh919 <101653588+Anirudh919@users.noreply.github.com>
2023-06-28 17:33:10 +0530

    compeleted

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+const sendMail = require("./controller/index");
+const app = express();
+app.use(bodyparser.json());
+app.use(
+  bodyparser.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+app.use(express.json());
+
+app.post("/", sendMail);
+
+app.listen(process.env.PORT || "3000", () => {
+  console.log(`server is running in port ${process.env.PORT || "3000"}`);
+});
